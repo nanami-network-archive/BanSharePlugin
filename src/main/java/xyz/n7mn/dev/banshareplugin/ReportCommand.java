3320ad7c2197b767aec59491b3bc5ccffc577514
kanasaki15 <kassi_0123@kassi-hp.info>
2021-04-15 20:30:57 +0900

    1.16.5対応準備

--- src/main/java/xyz/n7mn/dev/banshareplugin/ReportCommand.java
+++ src/main/java/xyz/n7mn/dev/banshareplugin/ReportCommand.java
@@ -1,16 +1,9 @@
 package xyz.n7mn.dev.banshareplugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.SkullType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 
 public class ReportCommand implements CommandExecutor {
     @Override
